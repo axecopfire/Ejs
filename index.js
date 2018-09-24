605be21b8d0b8b9004a7b91624a853270c712c23
axecopfire <axecopfire@users.noreply.github.com>
2018-09-24 19:01:31 -0400

    EJS CHP 7 Methods

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+=========================
+Code that starts @ Chapter 7 EJS
+========================
+*/
+
+
+// METHODS
+
+// function shout (deed) {
+//     console.log(`I, ${this.name} am all that is, and I shall ${deed}`);
+// };
+
+// let hunk = {name: "Carl", shout};
+// hunk.shout("vanquish all my foes");
+
+// -------------------------------------------
+
+// function attack (type) {
+//     console.log(`${this.hero} attacks with ${type}!`);
+// }
+// let warrior = {hero: "Vin the impaler", attack}
+// warrior.attack("impaling weapon thing")
+
+// PROTOTYPES
