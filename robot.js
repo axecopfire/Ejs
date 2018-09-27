06648200dafa074e4352aa4165536afbf95e31a2
axecopfire <sbraun990@gmail.com>
2018-09-26 22:28:20 -0400

    test case added at the bottom

--- robot.js
+++ robot.js
@@ -23,7 +23,7 @@ function buildGraph(edges) {
         addEdge(from, to);
         addEdge(to, from);
     }
-    console.log(graph);
+    // console.log(graph);
     return graph;
 }
 
@@ -56,3 +56,15 @@ class VillageState {
         }
     } 
 }
+
+/* Small little Test case that shows it works */
+/* let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's House"}]
+);
+
+let next = first.move("Alice's House");
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place); */
+
